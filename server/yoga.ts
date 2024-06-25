7f7282b9865a4b08da7cb363f08b6c685b9bebda
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-25 15:45:29 -0500

    Use codegen output in yoga

--- server/yoga.ts
+++ server/yoga.ts
@@ -1,5 +1,7 @@
 import { createSchema, createYoga } from 'graphql-yoga';
 
+import { Resolvers } from "./resolvers-types.ts";
+
 async function getTypeDefs(): Promise<string> {
   // Open schema file for reading
   using schemaFile = await Deno.open('./schema.graphql', { read: true });
@@ -16,14 +18,16 @@ async function getTypeDefs(): Promise<string> {
   return typeDefs;
 }
 
+const resolvers: Resolvers = {
+  Query: {
+    ok: () => true,
+  }
+}
+
 // Generate schema from file
 const schema = createSchema({
   typeDefs: await getTypeDefs(),
-  resolvers: {
-    Query: {
-      ok: () => true,
-    }
-  }
+  resolvers
 })
 
 export const  yoga = createYoga({ schema });
