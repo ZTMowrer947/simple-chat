2e678116292334af960547a08af8ec6003310943
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-26 09:44:00 -0500

    Setup urql client

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,17 @@
   </MainLayout>
 </template>
 
+<script lang="ts">
+import { cacheExchange, Client, fetchExchange, provideClient } from '@urql/vue';
+
+const client = new Client({
+  url: 'http://localhost:8000/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
+
+provideClient(client);
+</script>
+
 <script setup lang="ts">
 import MainLayout from './MainLayout.vue';
 </script>
