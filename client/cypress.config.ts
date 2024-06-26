608e9c336de0a4293cd6ba4d6e2e15dba8eb70de
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-26 08:48:38 -0500

    Setup component testing in Cypress

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "cypress";
 
+import webpackConfig from "./webpack.config";
+
 export default defineConfig({
   e2e: {
     // setupNodeEvents(on, config) {
-      // implement node event listeners here
+    // implement node event listeners here
     // },
+  },
+
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "webpack",
+      webpackConfig
+    },
   },
 });
