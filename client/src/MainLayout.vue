ef8bd638462c0a8015014ed17952b4e894139f75
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-26 09:53:46 -0500

    Erase all client subdirs
    Preparing revisal of previous setup

--- client/src/MainLayout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="w-full min-h-screen">
-    <slot></slot>
-  </div>
-</template>
--- client/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{ts,js,tsx,jsx,vue}', './src/template.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
