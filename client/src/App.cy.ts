f4bd05748e0371465e512274596b4a94a9246c89
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-26 09:06:52 -0500

    Add basic "text exists" check to App component test

--- client/src/App.cy.ts
+++ client/src/App.cy.ts
@@ -1,8 +1,12 @@
+/// <reference types="@testing-library/cypress" />
+
 import App from './App.vue'
 
 describe('<App />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
     cy.mount(App)
+
+    cy.findByText('Hello, world!').should('exist');
   })
 })
